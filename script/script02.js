7828235a9da80a0ed63646af2a03525fc32db553
shaiko_sv <foodprofi@gmail.com>
2019-08-21 19:54:34 -0300

    javaScript. Level 1 - Lesson 02 - corrected some errors informed by WebStorm

--- script/script02.js
+++ script/script02.js
@@ -1,5 +1,5 @@
 // 1. Дан код:
-var a = 1, b = 1, c, d;
+let a = 1, b = 1, c, d;
 c = ++a; alert(c);           // 2 сначала выполняется префикс ++a (a = a + 1), затем присвоение
 d = b++; alert(d);           // 1 сначала выполняется присвоение, затем постфикстый инкремент b (b=b+1)
 c = (2+ ++a); alert(c);      // 5 сначала a увеличится на один, затем значение прибавиться к 2
@@ -9,11 +9,12 @@ alert(b);                    // 3 см. выше
 // Почему код даёт именно такие результаты?
 
 // 2. Чему будет равен x в примере ниже?
-var a = 2;
-var x = 1 + (a *= 2); // = 1 + (a = a * 2) = 1 + (a = 2 * 2) = 1 + 4 = 5
+a = 2;
+let x = 1 + (a *= 2); // = 1 + (a = a * 2) = 1 + (a = 2 * 2) = 1 + 4 = 5
+console.log("x = ", x);
 // 3. Объявить две целочисленные переменные a и b и задать им произвольные начальные значения.
-var a = Math.round(Math.random()*200-100); console.log('a='+a);
-var b = Math.round(Math.random()*200-100); console.log('b='+b);
+a = Math.round(Math.random()*200-100); console.log('a='+a);
+b = Math.round(Math.random()*200-100); console.log('b='+b);
 // Затем написать скрипт, который работает по следующему принципу:
 // если a и b положительные, вывести их разность;
 if (a>=0 && b>=0)
@@ -63,16 +64,16 @@ switch (a) {
 // ### 5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
 function sum(a, b) {
     return a + b;
-};
+}
 function minus(a, b) {
     return a - b;
-};
+}
 function mult (a, b) {
     return a * b;
-};
+}
 function divid (a, b) {
     return a / b;
-};
+}
 console.log (sum(5, 7));
 console.log (minus(5, 7));
 console.log (mult(5, 7));
@@ -83,13 +84,10 @@ function mathOperation(arg1, arg2, operation) {
     switch (operation) {
         case "+":
             return sum(arg1, arg2);
-            break;
         case "-":
             return minus(arg1, arg2);
-            break;
         case "*":
             return mult(arg1, arg2);
-            break;
         case "/":
             return divid(arg1, arg2);
     }
@@ -112,13 +110,18 @@ console.log ("<=", null <= 0); //true
 // хорошее объяснение по ссылке https://learn.javascript.ru/comparison
 // ### 8) *С помощью рекурсии организовать функцию возведения числа в степень. Формат: function power(val, pow), где val – заданное число, pow – степень.
 function power(val, pow) {
-    var result;
-    if (pow == 0)
+    let result;
+    val = parseInt(val);
+    pow = parseInt(pow);
+    if (pow === 0) {
         return 1;
-    else if (pow == 1)
+    }
+    else if (pow === 1) {
         return val;
-    else
+    }
+    else {
         result = val * power(val, --pow);
+    }
     return result;
 }
 console.log ("power(2,64) = ",power(2,64));
