b3b71bb59cae6b0ab236521d68f9bbafdc30d5fb
shaiko_sv <foodprofi@gmail.com>
2019-10-09 21:51:57 +0300

    javaScript. Level 1 - Lesson 4 Напоминаю, что курс прохожу повторно, и иногда видео не смотрю полностью. Поэтому, если задание не написано, есть шанс, что я его не сделал. Досдаю, задание за 3 урок. Если с ним всё ок, то надеюсь на исправление оценки за прошлый урок. Игру Камень Ножницы Бумага сделав в стиле ООП.

--- script/app.js
+++ script/app.js
@@ -1,215 +1,196 @@
 "use strict";
+// !!!Задание с предыдущего урока!!!
 
-// 1. С помощью цикла while вывести все простые числа в промежутке от 0 до 100 (правильно в промежутке от 2 до 100)
-console.log('Задание 1');
-
-let x = 2, simpleNumbers = [];
-while (x <= 100) {
-    let controlNumber = x / 2, simple = true;
-    for (let i = 2; i < controlNumber + 1; i++) {
-        if (x % i === 0 && x !== 2) {
-            simple = false;
-            break;
-        }
-    }
-    if (simple) {
-         simpleNumbers.push(x);
-    }
-    x++;
-}
-console.log(simpleNumbers);
-
-// 2. Начиная с этого урока, мы начинаем работать с функционалом интернет-магазина.
-// Предположим, что у нас есть сущность корзины.
-// Нужно реализовать функционал подсчета стоимости корзины в зависимости от находящихся в ней товаров.
-// Товары в корзине хранятся в массиве.
-// 2.1. Организуйте такой массив для хранения товаров в корзине
-console.log('Задание 2');
-let productInCart = [{'name': 'Product1', 'quantity': 2, 'price': 5},
-                     {'name': 'Product2', 'quantity': 1, 'price': 10},
-                     {'name': 'Product3', 'quantity': 3, 'price': 15}];
-// 2.2. Организуйте функцию countBasketPrice, которая будет считать стоимость корзины.
-function countBasketPrice (productInCart) {
-    let cartAmount = 0;
-    for(let product of productInCart) {
-        cartAmount += (product.quantity * product.price);
-    }
-    return cartAmount;
+// 3. Практикум. Угадай число (БЕЗ ЦИКЛОВ) - рекурсия
+//     Напишем первую игру — «Угадай число». Браузер будет загадывать случайное четырехзначное число, а мы будем отгадывать.
+//     Попытки отгадать число будут идти через диалоговое окно — prompt.
+//          Браузер будет сообщать в ответ, больше или меньше загаданного наш ответ.
+//     Алгоритм будет таким:
+function gameNumber() {
+//  Браузер генерирует число и приглашает пользователя к игре.
+    let randomNumber = Math.round(Math.random()*10000);
+    console.log(randomNumber);
+    console.log('Добро пожаловать в игру "Угадай число"\n Я загадал 4-х значное число...\nЧтобы прикратить игру введите stop');
+    let counter = 0;
+    checkResult(randomNumber, counter);
 }
 
-console.log('Стоимость корзины: ' + countBasketPrice(productInCart));
-
-// 3. *Вывести с помощью цикла for числа от 0 до 9, НЕ используя тело цикла. То есть выглядеть должно вот так:
-// for(…){// здесь пусто}
-console.log('Задание 3');
-for (let i = 0; i < 10; console.log(i++)) {
-
-}
-// 4. * Нарисовать пирамиду с помощью console.log, как показано на рисунке,
-// только у вашей пирамиды должно быть 20 рядов, а не 5:
-// x
-// xx
-// xxx
-// xxxx
-// xxxxx
-console.log('Задание 4');
-for (let i = 1; i <= 20; i++) {
-    let j = i, x = '';
-    while (j) {
-        x += 'x';
-        j--;
+function checkResult(number, count) {
+//     Выводится окно запроса предположения.
+    let answer = prompt('Какое число я загадал?');
+    count ++;
+    //     Браузер проверяет число и возвращает результат.
+    if (answer == 'stop' || answer == 'STOP' || answer == 'Stop') {
+        console.log('Вы прервали игру...');
+        return count
+    } else if(number == answer) {
+//     Повторяем до тех пор, пока число не будет угадано.
+        console.log('Вы угадали! Число попыток ' + count);
+//     Как только число угадано, браузер сбрасывает число попыток и генерирует новое число.
+        gameNumber();
+    } else if(number > answer) {
+        console.log('Попытка ' + count + '. ' + answer + '. Больше...');
+        checkResult(number, count);
+    } else if(number < answer) {
+        console.log('Попытка ' + count + '. ' + answer + '. Мешьше...');
+        checkResult(number, count);
     }
-    console.log(x);
 }
 
-// Дополнительные задания
-console.log('Задание 5.2');
-// С помощью цикла for написать алгоритм для вывода чисел от 0 до 10 включительно, чтобы результат выглядел так:
-// 0 – это ноль
-// 1 – нечетное число
-// 2 – четное число
-// 3 – нечетное число
-// …
-// 10 – четное число
-for (let i = 0; i <= 10; i++) {
-    if (i == 0) {
-        console.log(`${i} - это ноль`);
-    } else if (i % 2 == 0) {
-        console.log(`${i} - четное число`);
+//  Пока не будем ничего выводить на страницу. И пока наш алгоритм далек от совершенства. Как только изучим новые возможности языка — сразу улучшим его.
+gameNumber();
+
+// Задания к уроку 4!!!
+
+// 1. Написать функцию, преобразующую число в объект.
+// Передавая на вход число от 0 до 999, мы должны получить на выходе объект,
+// в котором в соответствующих свойствах описаны единицы, десятки и сотни.
+// Например, для числа 245 мы должны получить следующий объект: {‘единицы’: 5, ‘десятки’: 4, ‘сотни’: 2}.
+// Если число превышает 999, необходимо выдать соответствующее сообщение с помощью console.log и вернуть пустой объект.
+
+let number;
+while (true) {
+    number = prompt('Введите число от 0 до 999');
+    if (isNaN(number) || number === '') {
+        alert('Пожалуйста введите число');
     } else {
-        console.log(`${i} - нечетное число`);
+        break;
     }
 }
 
-console.log('Задание 5.3');
-// Выведите в консоль значения, указанные рядом с комментариями:
-    const post = {
-        author: "John", //вывести этот текст
-        postId: 23,
-        comments: [
-            {
-                userId: 10,
-                userName: "Alex",
-                text: "lorem ipsum",
-                rating:{
-                    likes: 10,
-                    dislikes: 2 //вывести это число
-                }
-            },
-            {
-                userId: 5, //вывести это число
-                userName: "Jane",
-                text: "lorem ipsum 2", //вывести этот текст
-                rating: {
-                    likes: 3,
-                    dislikes: 1
-                }
-            },
-        ]
+function numberParts(number) {
+    let result = {};
+    let keys = ['сотни', 'десятки', 'единицы'];
+    let k = 0;
+    if (number.length == 1) {
+        k = 2
+    } else if (number.length == 2) {
+        k = 1
     }
-console.log(post.author); // "John"
-console.log(post.comments[0].rating.dislikes); // 2
-console.log(post.comments[1].userId); // 5
-console.log(post.comments[1].text); // "lorem ipsum 2"
-
-
-console.log('Задание 5.4');
-// Перед вами находится массив с продуктами, сегодня распродажа и вам нужно на каждый товар применить скидку 15%,
-// можете использовать метод forEach https://mzl.la/1AOMMWX :
-    const products1 = [
-        {
-            id: 3,
-            price: 200,
-        },
-        {
-            id: 4,
-            price: 900,
-        },
-        {
-            id: 1,
-            price: 1000,
-        },
-    ];
-    products1.forEach(function (element) {
-       element.price = element.price - element.price * 15 / 100
-           console.log(element)
-
-    });
-
-console.log('Задание 5.5');
-// Перед вами находится массив с продуктами в интернет-магазине. Вам нужно:
-//      1. Получить все товары, у которых есть фотографии, можете использовать метод filter https://mzl.la/2qROQkT
-//      2. Отсортируйте товары по цене (от низкой цены к высокой), можете использовать метод sort https://mzl.la/2Y79hbZ
-    const products2 = [
-        {
-            id: 3,
-            price: 127,
-            photos: [
-                "1.jpg",
-                "2.jpg",
-            ]
-        },
-        {
-            id: 5,
-            price: 499,
-            photos: []
-        },
-        {
-            id: 10,
-            price: 26,
-            photos: [
-                "3.jpg"
-            ]
-        },
-        {
-            id: 8,
-            price: 78,
-        },
-    ];
-
-    let productsWithPictureProperties = [];
-    // сначала находим все товары у которых есть свойство photos
-    for (let product of products2) {
-        if (Object.getOwnPropertyNames(product).includes('photos')) {
-            productsWithPictureProperties.push(product);
+    if (number > 999) {
+        console.log('число больше, чем 999');
+        return
+    } else {
+        for(let i = number.length - 1; i >= 0; i--) {
+            let key = keys[i + k];
+            result[key] = number[i];
         }
     }
-    // затем находим товары у которых есто свойство не пустое
-    let productsWithPicture = productsWithPictureProperties.filter(product => product.photos.length > 0 );
-
-    console.log(productsWithPicture);
-
-    products2.sort(function (a, b) {
-        if (a.price > b.price) {
-            return 1;
-        }
-        if (a.price < b.price) {
-            return -1;
-        }
-        return 0;
-    });
-
-    console.log(products2);
-
-console.log('Задание 5.6');
-// (По желанию, т.к. такая особенность практически не используется)
-// Вывести с помощью цикла for числа от 0 до 9, НЕ используя тело цикла.
-// То есть выглядеть должно примерно так:
-    for(let i = 0; i <= 9; console.log(i++)){/* здесь пусто */};
-
-console.log('Задание 5.7');
-// Нарисовать горку с помощью console.log (используя цикл for), как показано на рисунке,
-//    только у вашей горки должно быть 20 рядов, а не 5:
-//x
-//xx
-//xxx
-//xxxx
-//xxxxx
-for (let i = 1; i <= 20; i++) {
-    let j = i, x = '';
-    while (j) {
-        x += 'x';
-        j--;
-    }
-    console.log(x);
+    return result
 }
+
+console.log(numberParts(number));
+
+// 2. Реализовать собственный каталог товаров
+const IDS = [1,2,3,4];
+const ITEMS = ['milk','bread','juice','cheese'];
+const PRICES = [5, 2, 10, 15];
+let shop = {
+    catalog: [],
+    cart: [],
+    cartSum: 0,
+    createCatalog: function () {
+        for (let i = 0; i < ITEMS.length; i++) {
+            this.catalog.push(this._createProduct(IDS[i], ITEMS[i], PRICES[i]))
+        }
+    },
+    _createProduct: function(id, name, price) {
+        return {
+            id: id,
+            name: name,
+            price: price,
+        }
+    },
+    cartCalcSum: function () {
+        for (let i = 0; i < this.cart.length; i++) {
+            this.cartSum += this.cart[i].quantity * this.cart[i].product.price;
+        }
+    },
+    createCart: function () {
+        for (let i = 0; i < this.catalog.length; i++) {
+            this.cart.push(this._cartProductAdd(i))
+        }
+    },
+    _cartProductAdd: function (id, quantity=Math.round(Math.random()*10)) {
+        return {
+            product: this.catalog[(id)],
+            quantity: quantity,
+        }
+    }
+};
+
+shop.createCatalog();
+
+shop.createCart();
+
+shop.cartCalcSum();
+
+
+// 3. Доделать игру Камень-Ножницы-Бумага
+
+//КАМЕНЬ, НОЖНИЦЫ, БУМАГА
+// 1 - камень
+// 2 - ножницы
+// 3 - бумага
+
+let game = {
+    choiceAvailable: {1:'камень', 2:'ножницы', 3:'бумага'},
+    moveResultLegend: {1:'Вы победили', 2:'Ничья', 3:'Вы проиграли'},
+    moveCount: 0,
+    moveData: [],
+    score: {player:0, PC:0},
+    choicePC: [],
+    choicePlayer: [],
+    gameStart: function () {
+        alert('Добро пожаловать в игру "Камень-Ножницы-Будага"');
+        let playerName = prompt('Введите Ваше имя:');
+        while(true) {
+            this.move();
+            this.moveResult();
+            if (prompt('Продолжить - 1, Закончить - 2') == 2) {
+                alert(playerName + ', Вы сыграли: ' + this.moveCount + ' раз(а).\nСчёт: (Вы) ' + this.score.player + ' - ' + this.score.PC + ' (PC)');
+                break;
+            }
+        }
+    },
+    movePC: function () {
+        let PC = Math.floor(Math.random () * 3) + 1;
+        return PC;
+    },
+    movePlayer: function () {
+        let me = +prompt ('1 - камень \n2 - ножницы \n3 - бумага');
+        return me;
+    },
+    _moveRecord: function () {
+
+        return [this.movePlayer(), this.movePC()]
+    },
+    move: function f() {
+        this.moveCount++;
+        //this.moveData.push({[this.moveCount]: this._moveRecord()});
+        this.moveData.push(this._moveRecord());
+    },
+    moveResult: function () {
+        let result;
+        let me = this.moveData[this.moveCount-1][0];
+        let PC = this.moveData[this.moveCount-1][1];
+        if ((me === 1 && PC === 2) || (me === 2 && PC === 3) || (me === 3 && PC === 1)) {
+            result = 1;
+            this.score.player++;
+            console.log ('Вы победили! :)\nВы ' + this.choiceAvailable[me] + ', а компьютер ' + this.choiceAvailable[PC]);
+        } else if (me === PC) {
+            result = 2;
+            console.log ('НИЧЬЯ :O\nВы ' + this.choiceAvailable[me] + ', а компьютер ' + this.choiceAvailable[PC]);
+        } else {
+            result = 3;
+            this.score.PC++;
+            console.log ('Вы проиграли :(\nВы ' + this.choiceAvailable[me] + ', а компьютер ' + this.choiceAvailable[PC]);
+        }
+        return result;
+    }
+
+}
+game.gameStart();
+
+
+
