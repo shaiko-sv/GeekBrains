1a1687619402623f3c148a613e53acb17ad26635
shaiko_sv <foodprofi@gmail.com>
2019-11-27 20:24:46 +0300

    homework 5 JS level 2

--- project/js/main.js
+++ project/js/main.js
@@ -4,11 +4,12 @@ const app = new Vue({
     el: '#app',
     data: {
         catalogUrl: `/catalogData.json`,
+        cartUrl: `/getBasket.json`,
         products: [],
         allProducts: [],
         imgCatalog: `https://placehold.it/200x150`,
         imgCart: `https://placehold.it/50x100`,
-        show: false,
+        showCart: false,
         cartProducts: [],
         filtered: [],
         value: '',
@@ -20,14 +21,19 @@ const app = new Vue({
                 .catch(error => console.log(error))
         },
         addProduct(product){
-            let find = this.cartProducts.find(el => el.id_product === product.id_product);
-            if(find){
-                find.quantity++;
-            } else {
-                let prod = Object.assign({quantity: 1}, product);
-                this.cartProducts.push(prod);
-            }
-        },
+            this.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                        if (data.result) {
+                            let find = this.cartProducts.find(el => el.id_product === product.id_product);
+                            if (find) {
+                                find.quantity++;
+                            } else {
+                                let prod = Object.assign({quantity: 1}, product);
+                                this.cartProducts.push(prod);
+                            }
+                        }
+                    })},
+
         removeProduct(cartItem){
             let find = this.cartProducts.find(el => el.id_product === cartItem.id_product);
             if(find.quantity > 1){
@@ -36,24 +42,28 @@ const app = new Vue({
                 this.cartProducts.splice(this.cartProducts.indexOf(find), 1);
             }
         },
-        filter(value){
-            if(!value) {
+        filter() {
+            if(!this.value) {
                 this.products = [...this.allProducts];
             } else {
                 this.products = [];
-                const regexp = new RegExp(value, 'i');
+                let regexp = new RegExp(value, 'i');
                 this.filtered = this.allProducts.filter(el => regexp.test(el.product_name));
-                console.log(this.filtered);
                 this.allProducts.forEach(el => {
                     if(this.filtered.includes(el)){
                         this.products.push(el)
                     }
                 })
             }
-            console.log(this.products);
         }
     },
     mounted(){
+        this.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for(let el of data.contents){
+                    this.cartProducts.push(el);
+                }
+            });
         this.getJson(`${API + this.catalogUrl}`)
             .then(data => {
                 for(let el of data){
